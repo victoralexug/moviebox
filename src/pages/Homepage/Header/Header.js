f22fcce0e9f674f11613ab32177eb150e893db56
Victor Eze <74573591+victoralexug@users.noreply.github.com>
2024-04-15 09:39:00 +0000

    phone-edit

--- src/pages/Homepage/Header/Header.js
+++ src/pages/Homepage/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-function Header() {
+function Main() {
     return (
         <div className='header'>
             
@@ -63,4 +63,4 @@ function Header() {
     );
 }
 
-export default Header;
+export default Main;
